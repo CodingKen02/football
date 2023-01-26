252a6f7ddfa910baa6a90b754045cb2437cbaa12
CodingKen02 <71536021+CodingKen02@users.noreply.github.com>
2023-01-26 01:11:49 -0600

    Initial commit

---
+++ Football.java
@@ -0,0 +1,22 @@
+/*
+Name: Kennedy Keyes
+Date: February 18, 2021
+Name of Program: Football.java
+Description: This program will output "Football Madness is Here" 
+on separate lines.
+ */
+package football;
+/**
+ *
+ * @author codingken
+ */
+public class Football {
+// main method begins execution of Java application
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Football\nMadness\nis\nHere");
+    } // end method main
+    
+} // end class Football
